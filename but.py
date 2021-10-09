99280bd0a8c32e45f14ed03614f1ef5f45bf22c6
RITRITRIT <76897246+RITRITRIT@users.noreply.github.com>
2021-10-09 08:24:57 +0530

    updated bot.py and added JsonData func for attributes

--- but.py
+++ but.py
@@ -3,8 +3,23 @@ PATH = "C:\Program Files (x86)\chromedriver.exe" # Location of the webdriver fil
 
 driver = webdriver.Chrome(PATH)
 driver.implicitly_wait(15) 
-driver.get('https://web.whatsapp.com')
+driver.get('https://www.web.whatsapp.com')
 driver.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
 inputString = input("Enter message to send: ")
 while(True):
     driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(inputString)
+
+    
+    
+    def getJsonData(file, attr_ret, attr1, attr2, attr_val1, attr_val2):
+     
+    # Load the file's data in 'data' variable
+    data = json.load(file)
+    retv =[]
+ 
+    # If the attributes' value conditions are satisfied,
+    # append the name into the list to be returned.
+    for i in data:
+        if(i[attr1]== attr_val1 and i[attr2]== attr_val2):
+           retv.append(i[attr_ret])
+    return retv
